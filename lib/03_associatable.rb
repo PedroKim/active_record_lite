d9e329e15db2a895519d359324c0f18c78e145fb
Taihyung(Peter) Kim <kim.taihyung@gmail.com>
2023-10-04 12:31:21 -0400

    define Associatable module to extend to SQLObject to handle belongs_to and has_many associations

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,34 +10,49 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    self.foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    self.class_name = options[:class_name] || name.to_s.camelcase
+    self.primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    self.foreign_key = options[:foreign_key] || "#{self_class_name.downcase}_id".to_sym
+    self.class_name = options[:class_name] || name.to_s.singularize.camelcase
+    self.primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      foreign_key_val = send(options.send(:foreign_key))
+      where_hash = {}
+      where_hash[options.send(:primary_key)] = foreign_key_val
+      options.model_class.where(where_hash)[0]
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+    define_method(name) do
+      primary_key_val = send(options.send(:primary_key))
+      where_hash = {}
+      where_hash[options.send(:foreign_key)] = primary_key_val
+      options.model_class.where(where_hash)
+    end
   end
 
   def assoc_options
@@ -46,5 +61,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
